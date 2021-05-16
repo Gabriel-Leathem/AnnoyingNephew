d27fd1b062de235a21d6c18002d0c4b2fe68646f
Gabriel Leathem <gabsthedestroyer@gmail.com>
2021-05-16 14:59:41 +1000

    Update Algorithm1.js

--- Algorithm1.js
+++ Algorithm1.js
@@ -1,32 +1,30 @@
 let letters = ['D', 'A', 'C', 'D', 'A', 'C', 'E', 'B', 'D', 'A', 'E', 'A']
 
 function annoyingNephew(letters, moves) {
-	if (letters.length % 2) {
-		console.log("Number of letters are not even! STUPID PAUL!!!");
-	}
 	let index = 0
+		if (letters.length % 2) {
+ 			console.log("Number of letters are not even!");
+ 			return letters.length;
+ 		}
 	for (var i = 0; i < moves; i++) {
-		let currentLetter = letters[index];
-		if (currentLetter == 'A') {
+		switch(letters) {
+			case 'A':
 			index += 1;
-		}
-		if (currentLetter == 'B') {
+			break;
+			case 'B':
 			index += 4;
-		}
-		if (currentLetter == 'C') {
+			break;
+			case 'C':
 			index += 2;
-		}
-		if (currentLetter == 'D') {
+			break;
+			case 'D':
 			index += letters.length / 2;
-		}
-		if (currentLetter == 'E') {
+			break;
+			case 'E':
 			index -= 2;
-		}
-		if (index >= letters.length) {
-			index -= letters.length;
+			break;
 		}
 	}
 	return letters[index];
 }
-
 console.log(annoyingNephew(letters, 61));
